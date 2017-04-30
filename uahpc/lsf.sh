1edf86cb115b1f9bf8dc82ba66deeecd1cb4185d
Tyson L. Swetnam <tswetnam@cyverse.org>
2017-04-30 13:13:54 -0700

    Create lsf.sh

---
+++ uahpc/lsf.sh
@@ -0,0 +1,19 @@
+### This script asks for one entire node (span) on El Gato
+### To start script use `$ bsub lsf.sh` at the bash terminal
+
+###========================================
+#!/bin/bash
+#BSUB -R gpu
+#BSUB -n 16
+#BUSb -R "span[ptile=16]"
+#BSUB -x
+#BSUB -XF 
+#BSUB -Is bash
+#BSUB -o lsf.out
+#BSUB -e lsf.err
+#BSUB -q "windfall"
+#BSUB -J photoscan-pro
+#BSUB -u <User ID here>
+###----------------------------------------
+
+###end of script
